1d8a4727cb6fb39a1a792fa42f91c6583ab66827
Dmytro Rashko <dmitriy.rashko@amdocs.com>
2025-06-08 17:06:07 +0200

    fix build

--- python/src/kagent/models/vertexai/__init__.py
+++ python/src/kagent/models/vertexai/__init__.py
@@ -1,6 +1,6 @@
-from ._anthropic_vertex_client import AnthropicVertexAIChatCompletionClient
 from ._gemini_vertexai_client import GeminiVertexAIChatCompletionClient
-from .config import AnthropicVertexAIClientConfiguration, GeminiVertexAIClientConfiguration
+from ._anthropic_vertex_client import AnthropicVertexAIChatCompletionClient
+from .config import GeminiVertexAIClientConfiguration, AnthropicVertexAIClientConfiguration
 
 __all__ = [
     "GeminiVertexAIChatCompletionClient",
